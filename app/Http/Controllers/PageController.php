44fea17219c1b1583930d99df25365aa1dbda36d
Kevin Figuera <kevinalejandro3@gmail.com>
2021-07-13 15:44:30 -0400

    add: area publica

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Repository;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function home(){
+         $repositories = Repository::latest()->get();
+         return view('welcome',compact('repositories'));
+    }
+}
