a790984b247a8b7d3aea10163c04ea8e3973a8ee
Kevin Figuera <kevinalejandro3@gmail.com>
2021-07-13 12:04:47 -0400

    add: validación de campos

--- app/Http/Controllers/RepositoryController.php
+++ app/Http/Controllers/RepositoryController.php
@@ -2,20 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RepositoryRequest;
 use App\Models\Repository;
 use Illuminate\Http\Request;
 
 class RepositoryController extends Controller
 {
 
-    public function store(Request $request)
+    public function store(RepositoryRequest $request)
     {
         $request->user()->repositories()->create($request->all());
 
         return redirect()->route('repositories.index');
     }
 
-    public function update(Request $request, Repository $repository)
+    public function update(RepositoryRequest $request, Repository $repository)
     {
         $repository->update($request->all());
 
