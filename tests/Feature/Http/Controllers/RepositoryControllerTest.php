01f7ce2f1886ba222a6eedd9ab53fbe1e0fffb73
Kevin Figuera <kevinalejandro3@gmail.com>
2021-07-13 11:57:59 -0400

    add: actualizar registro

--- tests/Feature/Http/Controllers/RepositoryControllerTest.php
+++ tests/Feature/Http/Controllers/RepositoryControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Http\Controllers;
 
+use App\Models\Repository;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -10,6 +11,7 @@ use Tests\TestCase;
 class RepositoryControllerTest extends TestCase
 {
     use RefreshDatabase, WithFaker;
+
     public function test_guest()
     {
         $this->get('repositories')->assertRedirect('login');            //index
@@ -31,9 +33,26 @@ class RepositoryControllerTest extends TestCase
         $user = User::factory()->create();
 
         $this->actingAs($user)
-            ->post('repositories',$data)
+            ->post('repositories', $data)
             ->assertRedirect('repositories');
 
-        $this->assertDatabaseHas('repositories',$data);
+        $this->assertDatabaseHas('repositories', $data);
+    }
+
+    public function test_update()
+    {
+        $repository = Repository::factory()->create();
+        $data = [
+            'url' => $this->faker->url,
+            'description' => $this->faker->text,
+        ];
+
+        $user = User::factory()->create();
+
+        $this->actingAs($user)
+            ->put("repositories/{$repository->id}", $data)
+            ->assertRedirect("repositories/{$repository->id}/edit");
+
+        $this->assertDatabaseHas('repositories', $data);
     }
 }
